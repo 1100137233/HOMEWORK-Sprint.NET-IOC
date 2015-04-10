7b1383e14faca3c7d98e37047a3ce7c67047334a
circle <s1100137233@gm.kuas.edu.tw>
2015-04-10 22:50:39 +0800

    can reach

---
+++ KuasCore/Dao/Impl/NewEmployeeDao.cs
@@ -0,0 +1,36 @@
+﻿using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Dao.Impl
+{
+     public class NewEmployeeDao :IEmployeeDao
+    {
+         public IList<Employee> GetAllEmployees() {
+             List<Employee> allEmployee = new List<Employee>();
+             Employee e = new Employee();
+             e.Age= 11;
+             e.Name = "he";
+             e.Id = "1100";
+             allEmployee.Add(e);
+             return allEmployee;
+
+         }
+
+         public Employee GetEmployeeById(string id) {
+             Employee ee = new Employee();
+             ee.Id = "12";
+             ee.Name =id;
+             ee.Age = 15;
+
+             return ee;
+
+
+
+
+         }
+    }
+}
